4d4c843b7024a1c33452cff382fa19ecfeddc267
Wingzzzzz <14256490+Wingzzzzz@users.noreply.github.com>
2026-03-20 01:34:15 +0800

    remove unavailable query to resume live stream download from streamer main page

--- youtube_dl/extractor/twitch.py
+++ youtube_dl/extractor/twitch.py
@@ -47,7 +47,6 @@ class TwitchBaseIE(InfoExtractor):
         'FilterableVideoTower_Videos': 'a937f1d22e269e39a03b509f65a7490f9fc247d7f83d6ac1421523e3b68042cb',
         'ClipsCards__User': 'b73ad2bfaecfd30a9e6c28fada15bd97032c83ec77a0440766a56fe0bd632777',
         'ChannelCollectionsContent': '07e3691a1bad77a36aba590c351180439a40baefc1c275356f40fc7082419a84',
-        'StreamMetadata': '1c719a40e481453e5c48d9bb585d971b8b372f8ebb105b17076722264dfa5b3e',
         'ComscoreStreamingQuery': 'e1edae8122517d013405f237ffcc124515dc6ded82480a88daef69c83b53ac01',
         'VideoAccessToken_Clip': '36b89d2507fce29e5ca551df756d27c1cfe079e2609642b4390aa4c35796eb11',
         'VideoPreviewOverlay': '3006e77e51b128d838fa4e835723ca4dc9a05c5efd4466c1085215c6e437e65c',
@@ -766,9 +765,6 @@ class TwitchStreamIE(TwitchBaseIE):
 
         gql = self._download_gql(
             channel_name, [{
-                'operationName': 'StreamMetadata',
-                'variables': {'channelLogin': channel_name},
-            }, {
                 'operationName': 'ComscoreStreamingQuery',
                 'variables': {
                     'channel': channel_name,
